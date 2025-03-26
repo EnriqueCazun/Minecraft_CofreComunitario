aa424b66b949ee9c66ae68482b68585892528906
Enrique Cazun <enriquecazu@gmail.com>
2025-03-25 20:40:14 -0600

    Update CommunityChest.java
    Ahora el contenido del cofre se mantiene despues de que el servidor se apague o se caiga

--- CofreComunitario/src/main/java/com/Cofre/CommunityChest.java
+++ CofreComunitario/src/main/java/com/Cofre/CommunityChest.java
@@ -6,6 +6,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class CommunityChest extends JavaPlugin {
 
@@ -13,15 +18,48 @@ public class CommunityChest extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getDataFolder().mkdirs();
         communityChest = Bukkit.createInventory(null, 54, "Cofre Comunitario");
+        loadChest();
         getLogger().info("Plugin de Cofre Comunitario activado!");
     }
 
     @Override
     public void onDisable() {
+        saveChest();
         getLogger().info("Plugin de Cofre Comunitario desactivado!");
     }
+    private void saveChest() {
+        try {
+            File file = new File(getDataFolder(), "chest.yml");
+            YamlConfiguration config = new YamlConfiguration();
+            
+            config.set("chest", communityChest.getContents());
+            
+            config.save(file);
+        } catch (IOException e) {
+            getLogger().severe("¡Error al guardar el cofre comunitario!");
+            e.printStackTrace();
+        }
+    }
 
+    private void loadChest() {
+        File file = new File(getDataFolder(), "chest.yml");
+        if (file.exists()) {
+            try {
+                YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+                
+                List<ItemStack> items = (List<ItemStack>) config.getList("chest");
+                
+                if (items != null) {
+                    communityChest.setContents(items.toArray(new ItemStack[0]));
+                }
+            } catch (Exception e) {
+                getLogger().severe("¡Error al cargar el cofre comunitario!");
+                e.printStackTrace();
+            }
+        }
+    }
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("cc")) {
